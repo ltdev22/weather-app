469b4d329ae8e60fa691fe9bfef469c76e502c40
Lykourgos Tsirikos <lykourgos.tsirikos@gmail.com>
2020-04-04 20:44:19 +0100

    Installing Express.js and setting routes

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-const geoCode = require('./utils/geocode');
-const forecast = require('./utils/forecast');
+const express = require('express');
+const app = express();
 
-// Getting today's weather forecast for a given location
-const givenLocation = process.argv[2];
-if (!givenLocation) {
-    return console.log('Please type a location.');
-}
+// Setting routes
 
-geoCode(givenLocation)
-    .then(response => {
-        let { latitude, longitude, location } = response;
-        forecast(latitude, longitude)
-            .then(response => {
-                console.log(location);
-                console.log(response);
-            })
-            .catch(error => console.log(error));
-    })
-    .catch(error => console.log(error));
-console.log("Today's weather forecast:");
-// This prints the following on the terminal
-//
-// Today's weather forecast:
-// Bristol, City Of Bristol, England, United Kingdom
-// It is currently 7.85 degrees out. There's a 0% chance to rain.
+// Home page
+app.get('', (req, res) => {
+    res.send('Hello Express.js!');
+});
+
+// Help page
+app.get('/help', (req, res) => {
+    res.send('This is the Help page');
+});
+
+// Weather page
+app.get('/weather', (req, res) => {
+    res.send({
+        location: 'Bristol, U.K.',
+        forecast: 'The weather is fine'
+    });
+});
+
+// About page
+app.get('/about', (req, res) => {
+    res.send('<h1>This is the about page</h1>');
+});
+
+app.listen(8080, () => console.log(`Server is running on port 8080`));
