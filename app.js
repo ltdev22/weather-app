90823990bb7c8c6d0f7c048029d420517693c118
Lykourgos Tsirikos <lykourgos.tsirikos@gmail.com>
2020-04-05 11:55:05 +0100

    Serving up static files

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
+const path = require('path');
 const express = require('express');
 const app = express();
+
+// Define paths for Express config
+const publicDirPath = path.join(__dirname, '/public');
+
+// Configure Express to point to the public dir
+app.use(express.static(publicDirPath));
 
 // Setting routes
 
 // Home page
-app.get('', (req, res) => {
-    res.send('Hello Express.js!');
-});
+// app.get('', (req, res) => {
+//     res.send('Hello Express.js!');
+// });
 
 // Help page
-app.get('/help', (req, res) => {
-    res.send('This is the Help page');
-});
+// app.get('/help', (req, res) => {
+//     res.send('This is the Help page');
+// });
+
+// About page
+// app.get('/about', (req, res) => {
+//     res.send('<h1>This is the about page</h1>');
+// });
 
 // Weather page
 app.get('/weather', (req, res) => {
@@ -19,11 +31,6 @@ app.get('/weather', (req, res) => {
         location: 'Bristol, U.K.',
         forecast: 'The weather is fine'
     });
-});
-
-// About page
-app.get('/about', (req, res) => {
-    res.send('<h1>This is the about page</h1>');
 });
 
 app.listen(8080, () => console.log(`Server is running on port 8080`));
