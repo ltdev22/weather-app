323d95d51ac9aa70baada507f45ddb32fd3fea5e
Lykourgos Tsirikos <lykourgos.tsirikos@gmail.com>
2020-04-07 21:56:07 +0100

    Wrap up search functionality

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,6 +2,11 @@ console.log('client-side JS');
 
 const weatherForm = document.querySelector('#weather-form');
 const searchTerm = document.querySelector('#search');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+messageOne.classList.add('hidden');
+messageTwo.classList.add('hidden');
 
 weatherForm.addEventListener('submit', event => {
     event.preventDefault();
@@ -11,12 +16,18 @@ weatherForm.addEventListener('submit', event => {
         response => {
             response.json().then(data => {
                 if (data.error) {
-                    console.log(data.error);
+                    messageTwo.classList.add('hidden');
+                    messageOne.classList.remove('hidden');
+                    messageOne.textContent = data.error;
                 } else if (data.forecast.error) {
-                    console.log(data.forecast.error);
+                    messageTwo.classList.add('hidden');
+                    messageOne.classList.remove('hidden');
+                    messageOne.textContent = data.forecast.error;
                 } else {
-                    console.log(data.location);
-                    console.log(data.forecast);
+                    messageOne.classList.remove('hidden');
+                    messageTwo.classList.remove('hidden');
+                    messageOne.textContent = data.location;
+                    messageTwo.textContent = data.forecast;
                 }
             });
         }
